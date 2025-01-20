38f076eea1bd4560a0d8d1f25dd7a99f585648d4
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2025-01-20 08:19:37 +0700

    Updated middleware according to Clerk updated documentation

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,47 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  ClerkMiddlewareAuth,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware( async (auth, req) => {
-    if(!isPublicRoute(req)) await auth.protect();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn, orgId } = await auth();
+
+  // protect and redirect non-authenticated users from protected routes
+  if (!userId && !isPublicRoute(req)){
+    return redirectToSignIn();
   }
-);
+
+  // redirect authenticated users out of public routes
+  if (userId && isPublicRoute(req)){
+    // if organization not selected, redirect to organization selection
+    let path = "/select-org";
+
+    // if organization is selected, redirect to organization page
+    if (orgId){
+      let path = "/organization/${orgId}";
+    }
+
+    const orgRedirect = new URL(path, req.url);
+    return NextResponse.redirect(orgRedirect);
+  }
+
+  // redirect authenticated users without an organization yet to select organization page
+  // to force organization selection
+  if (userId && !orgId && req.nextUrl.pathname !== '/select-org'){
+    return NextResponse.redirect(new URL('/select-org', req.url));
+  }
+
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
