b9917b43011e6f0427fbb61c5b4df3ea72f69db5
Ye Thi Ha Htwe <108426020+yethihahtwe@users.noreply.github.com>
2025-02-10 17:09:16 +0000

    Tue, Feb 11, 2025, 12:09 AM +07:00

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    allowedOrigins: [
+      'loalhost:3000',
+      '.app.github.dev'
+    ]
+  }
 };
 
 export default nextConfig;
