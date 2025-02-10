fe21065f61bb3587ed47d50f9d3160524953fe62
Ye Thi Ha Htwe <108426020+yethihahtwe@users.noreply.github.com>
2025-02-11 00:16:56 +0700

    updated the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverActions: {
-    allowedOrigins: [
-      'loalhost:3000',
-      '.app.github.dev'
-    ]
-  }
 };
 
 export default nextConfig;
